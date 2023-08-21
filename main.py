4194f6d5b92115ec528fd8db0e022faeb56c33db
Bruno Henrique Silva de Oliveira <47747909+BrunoChin@users.noreply.github.com>
2023-08-21 19:28:07 -0300

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/view')
+def view():
+    df = load_dataFrame()
+    values = df.head().values.tolist()
+    return render_template('view.html', labels=df.keys(), values=values)
+
+def load_dataFrame():
+    df = pd.read_csv("static/exemple.csv")
+
+    return df
+
+app.run(debug=True)
